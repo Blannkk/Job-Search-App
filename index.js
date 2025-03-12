d22ff3093bcce215e4b3ce6ef63356b30ea860f5
ahmed <ahmed.a.gad33@gmail.com>
2025-03-12 17:32:40 +0200

    update index file

--- index.js
+++ index.js
@@ -1,14 +1,13 @@
 import express from "express";
 import bootstrap from "./src/app.controller.js";
 import { initSocket } from "./src/socketio/index.js";
-
 const app = express();
-const port = process.env.PORT || 3000;
 
+const port = process.env.PORT || 3000;
 bootstrap(app, express);
 
-const server = app;
+const server = app.listen(port, () =>
+  console.log("Example app listening on port port!", port)
+);
 
 initSocket(server);
-
-export default app;
